695bb52a3b52e08146b142bb3ba023c240c8f794
dinhhuyhuy <tdhuy21@clc.fitus.edu.vn>
2024-04-14 22:39:58 +0700

    Finsh manage customer

---
+++ BookStoreManager/Process/CustomerManagerBus.cs
@@ -0,0 +1,32 @@
+﻿using BookStoreManager.Database;
+using BookStoreManager.DataType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreManager.Process
+{
+    public class CustomerManagerBus
+    {
+        public CustomerDao Dao { get; set; }
+        public CustomerManagerBus() { 
+            Dao = new CustomerDao();
+        }
+        public void AddCustomer(CustomerModel customer)
+        {
+            Dao.InsertCustomerIntoDB(customer);
+        }
+
+        public void UpdateCustomer(CustomerModel customer) 
+        {
+            Dao.UpdateCustomerToDB(customer);
+        }
+
+        public void DeleteCustomer(CustomerModel customer)
+        {
+            Dao.DeleteCustomerFromDB(customer);
+        }
+    }
+}
--- BookStoreManager/UI/CustomerWindow.xaml.cs
+++ BookStoreManager/UI/CustomerWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using BookStoreManager.DataType;
 using BookStoreManager.Process;
+using DocumentFormat.OpenXml.Drawing.Diagrams;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using DocumentFormat.OpenXml.Wordprocessing;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -49,17 +52,49 @@ namespace BookStoreManager.UI
 
         private void addButton_Click(object sender, RoutedEventArgs e)
         {
-
+            AddCustomerWindow window = new AddCustomerWindow();
+            window.Closed += ManageCustomerWindow_Closed;
+            window.Show();
         }
 
         private void updateButton_Click(object sender, RoutedEventArgs e)
         {
-
+            var selected = CustomerDataGrid.SelectedItem as CustomerModel;
+            if (selected == null)
+            {
+                MessageBox.Show("Vui lòng chọn 1 khách hàng.");
+                return;
+            }
+            UpdateCustomerWindow window = new UpdateCustomerWindow((CustomerModel)selected.Clone());
+            window.Closed += ManageCustomerWindow_Closed;
+            window.Show();
         }
-
+        private void ManageCustomerWindow_Closed(object sender, EventArgs e)
+        {
+            LoadCustomerList();
+        }
         private void deleteButton_Click(object sender, RoutedEventArgs e)
         {
-
+            var selected = CustomerDataGrid.SelectedItem as CustomerModel;
+            if (selected == null)
+            {
+                MessageBox.Show("Vui lòng chọn 1 khách hàng.");
+                return;
+            }
+            var messbox = MessageBox.Show($"Bạn có chắc muốn xóa khách hàng {selected.CustomerName}, id = {selected.CustomerID}?"
+                , $"Xóa khách hàng {selected.CustomerName}?", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            if (messbox == MessageBoxResult.OK)
+            {
+                try
+                {
+                    CustomerManagerBus bus = new CustomerManagerBus();
+                    bus.DeleteCustomer(selected);
+                    MessageBox.Show($"Xóa danh mục {selected.CustomerName} thành công.");
+                    Bus.RefreshPage();
+                    LoadCustomerList();
+                }
+                catch (Exception ex) { MessageBox.Show($"Xóa danh mục {selected.CustomerName} thất bại."); }
+            }
         }
 
         private void prevButton_Click(object sender, RoutedEventArgs e)
